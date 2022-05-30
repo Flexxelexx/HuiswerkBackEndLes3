1242fbd4ea9f883e66c480bbe26f0d417e8136c6
Flexxelexx <alexxkooij@hotmail.com>
2022-05-30 09:03:51 +0200

    second draft

---
+++ Translator.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+
+public class Translator {
+
+    HashMap<Integer, String> numericAlpha = new HashMap<Integer, String>(); {
+
+    }
+
+    public Translator(String[] alphabetic, Integer[] numeric) {
+
+
+
+
+        for (int i = 0; i < alphabetic.length; i++) {
+            System.out.println(alphabetic[i]);
+        }
+    }
+//
+//    public translate (Integer number) {
+//        return String;
+//    }
+
+}
+
