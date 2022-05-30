284f2f75c545ac75ac9bfb1ad6072e9d4bf90877
Flexxelexx <alexxkooij@hotmail.com>
2022-05-30 09:36:47 +0200

    third draft

---
+++ Main.java
@@ -0,0 +1,24 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main (String[] args) {
+
+        boolean play = true;
+
+        String ongeldig = "ongeldige invoer";
+
+        Scanner scanner = new Scanner(System.in);
+
+        Integer[] numeric = {1,2,3,4,5,6,7,8,9,0};
+
+
+        String[] alphabetic = {"een, twee, drie, vier, vijf, zes, zeven, acht, negen, nul"};
+    }
+
+//    Translator translate = new Translator(); {
+//        translate =
+//    }
+}
--- Translator.java
+++ Translator.java
@@ -4,21 +4,36 @@ public class Translator {
 
     HashMap<Integer, String> numericAlpha = new HashMap<Integer, String>(); {
 
+        numericAlpha.put(1, "een");
+        numericAlpha.put(2, "twee");
+        numericAlpha.put(3, "drie");
+        numericAlpha.put(4, "vier");
+        numericAlpha.put(5, "vijf");
+        numericAlpha.put(6, "zes");
+        numericAlpha.put(7, "zeven");
+        numericAlpha.put(8, "acht");
+        numericAlpha.put(9, "negen");
+        numericAlpha.put(0, "nul");
+    }
+
+    public Translator() {
+
     }
 
     public Translator(String[] alphabetic, Integer[] numeric) {
 
 
-
-
         for (int i = 0; i < alphabetic.length; i++) {
+
+            numericAlpha.put(11, "elf");
             System.out.println(alphabetic[i]);
         }
     }
-//
-//    public translate (Integer number) {
-//        return String;
-//    }
+
+    public Integer translate(Integer number) {
+        return number;
+    }
+
 
 }
 
